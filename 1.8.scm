3b5b545a2257fe9d2930d46531707c9427edc690
Simon <fuaiqiang@gmail.com>
2019-04-06 22:27:07 +0800

    Update exercise 1.8

--- 1.8.scm
+++ 1.8.scm
@@ -1,35 +1,35 @@
-(define (abs x)
-  (if (< x 0)
+(define (cube-root x)
+  (define (abs x)
+    (if (< x 0)
       (- x)
       x))
 
-(define (square x) (* x x))
+  (define (square x) (* x x))
 
-(define (cube x) (* x x x))
-
-(define (average x y)
-  (/ (+ x y)
-     2))
-
-(define (good-enough? guess x)
-  (< (/ (abs (- (cube guess)
-                x))
-        x)
-     0.00001))
+  (define (cube x) (* x x x))
   
-(define (improve guess x)
-  (average guess (/ (+ (/ x
-                          (square guess))
-                        (* 2 guess))
-                    3)))
+  (define (average x y)
+    (/ (+ x y)
+       2))
+  
+  (define (good-enough? guess)
+    (< (/ (abs (- (cube guess)
+                x))
+          x)
+       0.00001))
 
-(define (cube-root-iter guess x)
-  (if (good-enough? guess x)
+  (define (improve guess)
+      (average guess (/ (+ (/ x
+                              (square guess))
+                           (* 2 guess))
+                        3)))
+
+  (define (cube-root-iter guess)
+    (if (good-enough? guess)
       guess
-      (cube-root-iter (improve guess x) x)))
+      (cube-root-iter (improve guess))))
 
-(define (cube-root x)
-  (cube-root-iter 1.0 x))
+  (cube-root-iter 1.0))
 
 (cube-root 8)
 (cube-root 0.000001)
