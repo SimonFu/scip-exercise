2faa738adc201f1dbbf408cfe1e95decd81d4e32
Simon <aiqiang.fu@hp.com>
2019-04-15 15:48:25 +0800

    Update it

--- 2.20.scm
+++ 2.20.scm
@@ -3,11 +3,10 @@
         (define (iter l)
             (let ((r (remainder x 2)))
                 (cond   ((null? l) '())
-                        ((= (remainder (car l) 2) r) (append (list (car l)) (iter (cdr l))))
+                        ((= (remainder (car l) 2) r) (cons (car l) (iter (cdr l))))
                         (else (iter (cdr l))))))
 
-        (append (list x) (iter l))))
+        (cons x (iter l))))
 
 (same-parity 1 2 3 4 5 6 7)
 (same-parity 2 3 4 5 6 7)
-
