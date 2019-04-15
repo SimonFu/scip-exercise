d00acb867a752c7aa0c3a3ed94eb1ddbabb47e52
Simon <aiqiang.fu@hp.com>
2019-04-15 14:32:45 +0800

    Update it

---
+++ utilities/fixed-point.scm
@@ -0,0 +1,11 @@
+(define (fixed-point f first-guess)
+    (define tolerance 0.000001)
+    (define (close-enough? x y)
+        (< (abs (- x y)) tolerance))
+    (define (try guess)
+        (let ((next (f (f guess))))
+            (if (close-enough? guess next) 
+                (/ (+ guess next) 2)
+                (try next))))
+                
+    (try first-guess))
