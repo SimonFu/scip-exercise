7adb1401b55543847e62aff2e7e9e50d5202dd44
Simon <aiqiang.fu@hp.com>
2019-04-18 18:02:18 +0800

    Do exercise

--- 2.37.scm
+++ 2.37.scm
@@ -12,3 +12,11 @@
     (accumulate-n cons '() m))
 
 (transpose (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+
+(define (matrix-*-matrix m n)
+    (let ((cols (transpose n)))
+        (map (lambda (x)
+            (map (lambda (y)
+                    (dot-product x y)) cols)) m)))
+
+(matrix-*-matrix (list (list 1 2 3) (list 4 5 6) (list 6 7 8)) (list (list 1 2 3) (list 4 5 6) (list 6 7 8)))
