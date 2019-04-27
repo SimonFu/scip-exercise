8ac505b0bdd62ae590b18ffc8de9169c625ffb11
Simon <fuaiqiang@gmail.com>
2019-04-27 14:18:01 +0800

    u

--- 2.69.scm
+++ 2.69.scm
@@ -6,17 +6,21 @@
                 (if (< (weight x) (weight tree))
                     (make-code-tree x tree)
                     (make-code-tree tree x)))
-            ((< (weight x) (weight tree)) (make-code-tree   (adjoin-tree x (left-branch tree))
-                                                            (right-branch tree)))
-            ((else (make-code-tree   (adjoin-tree x (right-branch tree)
-                                                    (left-branch tree)))))))
+            ((> (weight x) (weight tree)) (make-code-tree   tree
+                                                            x))
+            (else (make-code-tree   (adjoin-tree x  (left-branch tree))
+                                                    (right-branch tree)))))
 
 (define (successive-merge set)
     (define (iter set tree)
         (if (null? set) tree
                         (let ((x (car set)))
-                            (iter (cdr set) (adjouin-tree x tree)))))
+                            (iter (cdr set) (adjoin-tree x tree)))))
     (iter (cddr set) (make-code-tree (car set) (cadr set))))
 
 (define (generate-huffman-tree pairs)
     (successive-merge (make-leaf-set pairs)))
+
+(define l (list (list 'C 1) (list 'D 1) (list 'B 2) (list 'A 4)))
+
+(generate-huffman-tree l)
